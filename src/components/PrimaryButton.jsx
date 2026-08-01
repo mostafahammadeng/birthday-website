4ea7ee784ccfef0dada4ca5ce43ae15e89d99fa9
mostafahammadeng <mostafahammad.25.3@gmail.com>
2026-08-01 13:07:36 +0300

    Update PrimaryButton.jsx

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,10 +1,1 @@
-function PrimaryButton({ children, icon: Icon, ...props }) {
-  return (
-    <button className="primary-button" type="button" {...props}>
-      {Icon && <Icon />}
-      <span>{children}</span>
-    </button>
-  );
-}
 
-export default PrimaryButton;
